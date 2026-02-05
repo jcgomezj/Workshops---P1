21030b83e74d4c2bb44307f1defdafc030aae336
Camilo Gomez <jcgomezj@eafit.edu.co>
2026-02-05 11:23:26 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home (request):
-    return render(request, 'home.html',{'name':'Greg Lim'})
+    return render(request, 'home.html',{'name':'Camilo Gomez'})
 
 
 def about (request):
